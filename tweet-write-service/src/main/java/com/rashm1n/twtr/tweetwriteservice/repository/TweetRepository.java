a7d01a74e53d8cebcaeae10def1c36386164666f
rashm1n <rashmin.ravindu@gmail.com>
2023-05-22 23:19:18 +0530

    Formatting changes

--- tweet-write-service/src/main/java/com/rashm1n/twtr/tweetwriteservice/repository/TweetRepository.java
+++ tweet-write-service/src/main/java/com/rashm1n/twtr/tweetwriteservice/repository/TweetRepository.java
@@ -3,10 +3,7 @@ package com.rashm1n.twtr.tweetwriteservice.repository;
 import com.rashm1n.twtr.tweetwriteservice.model.Tweet;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.UUID;
 
 @Repository
 public interface TweetRepository extends CrudRepository<Tweet, String> {
