38d794ce621e0b9a79be797236a50d93fb4702c1
rashm1n <rashmin.ravindu@gmail.com>
2023-09-02 22:14:17 +0530

    Adding environment variables for bootstrap servers

--- fanout-service/src/main/java/com/rashm1n/twtr/fanoutservice/config/KafkaConsumerConfig.java
+++ fanout-service/src/main/java/com/rashm1n/twtr/fanoutservice/config/KafkaConsumerConfig.java
@@ -1,11 +1,9 @@
 package com.rashm1n.twtr.fanoutservice.config;
 
-import com.rashm1n.twtr.fanoutservice.model.FanOutMessageDTO;
 import com.rashm1n.twtr.fanoutservice.model.TweetMessageDTO;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -16,10 +14,13 @@ import java.util.Map;
 
 @Configuration
 public class KafkaConsumerConfig {
+    @Value(value="${spring.kafka.bootstrap-servers}")
+    public String bootstrapServers;
+
     @Bean
     public Map<String, Object> consumerProps() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:19092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,TweetDeserializer.class);
         return props;
