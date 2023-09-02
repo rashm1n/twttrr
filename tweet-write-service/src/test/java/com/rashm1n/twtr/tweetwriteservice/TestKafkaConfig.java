39d3662ade0f0b3294a205ee7cf10e9fabc118a4
rashm1n <rashmin.ravindu@gmail.com>
2023-09-03 01:12:31 +0530

    Adding testing configurations

--- tweet-write-service/src/test/java/com/rashm1n/twtr/tweetwriteservice/TestKafkaConfig.java
+++ tweet-write-service/src/test/java/com/rashm1n/twtr/tweetwriteservice/TestKafkaConfig.java
@@ -1,19 +1,19 @@
-package com.rashm1n.twtr.tweetwriteservice.config;
+package com.rashm1n.twtr.tweetwriteservice;
 
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.config.TopicBuilder;
 
-@Configuration
-@Profile("!Test")
-public class TopicConfig {
+@TestConfiguration
+@Profile("Test")
+public class TestKafkaConfig {
     @Value("${tweet-write-svc.kafka.topic}")
     private String topic;
     @Bean
     public NewTopic tweetTopic() {
-        return TopicBuilder.name(topic).partitions(3).replicas(3).build();
+        return TopicBuilder.name(topic).partitions(1).replicas(1).build();
     }
 }
--- tweet-write-service/src/test/java/com/rashm1n/twtr/tweetwriteservice/TweetWriteServiceApplicationTests.java
+++ tweet-write-service/src/test/java/com/rashm1n/twtr/tweetwriteservice/TweetWriteServiceApplicationTests.java
@@ -1,11 +1,54 @@
 package com.rashm1n.twtr.tweetwriteservice;
 
+import com.rashm1n.twtr.tweetwriteservice.controller.TweetController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.util.function.SupplierUtils;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.OracleContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest class TweetWriteServiceApplicationTests {
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-    @Test void contextLoads() {
+@SpringBootTest
+@Testcontainers
+@ActiveProfiles("Test")
+@Import(TestKafkaConfig.class)
+class TweetWriteServiceApplicationTests {
+
+    @Container
+    static final KafkaContainer kafka = new KafkaContainer(
+            DockerImageName.parse("confluentinc/cp-kafka:latest")
+    ).withKraft();
+
+    @Container
+    static final OracleContainer oracle = new OracleContainer(
+            DockerImageName.parse("gvenzl/oracle-xe:21-slim-faststart")
+    ).withDatabaseName("testDB").withUsername("testUser").withPassword("testPwd");
+
+    @DynamicPropertySource
+    static void overrideProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+        registry.add("spring.datasource.url", oracle::getJdbcUrl);
+        registry.add("spring.datasource.username", oracle::getUsername);
+        registry.add("spring.datasource.password", oracle::getPassword);
+        registry.add("tweet-write-svc.kafka.topic", ()->"tweet");
+        registry.add("server.port", ()->8080);
+    }
+
+    @Autowired
+    private TweetController controller;
+
+    @Test
+    void contextLoads() {
+        assertNotNull(controller);
     }
 
 }
